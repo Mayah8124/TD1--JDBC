68e61914ea14502ca2c66ce0e4089f5f908aee52
mayahNeko <mayahibela@gmail.com>
2025-12-06 18:00:14 +0300

    feat: Add initialization scripts to init_db.sql

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE product_management_db;
+CREATE USER product_manager_user
+    WITH PASSWORD '123456';
+\c rpoduct_management_db;
+GRANT CONNECT ON DATABASE product_management_db
+    TO product_manager_user;
+GRANT USAGE ON SCHEMA public
+    TO product_manager_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT ALL ON TABLES TO product_manager_user;
+GRANT SELECT , INSERT , UPDATE , DELETE ON ALL TABLES IN SCHEMA public
+    TO product_manager_user;
+GRANT CREATE ON SCHEMA public
+    TO product_manager_user;
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public
+    TO product_manager_user;
