9ce656d145fe8c5897755c1eaf50bdaf3709acf0
mayahNeko <mayahibela@gmail.com>
2025-12-06 18:59:00 +0300

    feat: Rename DatabaseConnection

--- src/main/java/config/DBConnection.java
+++ src/main/java/config/DBConnection.java
@@ -2,12 +2,12 @@ package config;
 
 import java.sql.*;
 
-public class DatabaseConnection {
+public class DBConnection {
     private final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
     private final String USER = "postgres";
     private final String PASSWORD = "nekoko";
 
-    public Connection getConnection() throws SQLException {
+    public Connection getDBConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
