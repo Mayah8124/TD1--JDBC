a21dff1b0f796919a18edf104f4946336f3165a9
mayahNeko <mayahibela@gmail.com>
2025-12-06 16:20:04 +0300

    feat: add main classes

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,33 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private LocalDateTime creation_datetime;
+
+    public Product(int id , String name , double price , LocalDateTime creation_datetime) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.creation_datetime = creation_datetime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreation_datetime() {
+        return creation_datetime;
+    }
 }
--- src/main/java/model/Product_category.java
+++ src/main/java/model/Product_category.java
@@ -1,4 +1,36 @@
 package model;
 
 public class Product_category {
+    private int id;
+    private String name;
+    private Product product_id;
+
+    public Product_category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product getProduct_id() {
+        return product_id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setProduct_id(Product product_id) {
+        this.product_id = product_id;
+    }
 }
