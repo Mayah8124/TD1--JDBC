12bf2a98b1e46355596ca0a9dde04ecab462df27
mayahNeko <mayahibela@gmail.com>
2025-12-09 21:02:37 +0300

    feat: Add test of getProductList() method

--- src/main/java/com/JDBC/app/Main.java
+++ src/main/java/com/JDBC/app/Main.java
@@ -2,6 +2,7 @@ package com.JDBC.app;
 
 import com.JDBC.controller.DataRetriever;
 import com.JDBC.model.Category;
+import com.JDBC.model.Product;
 
 import java.util.List;
 
@@ -12,6 +13,13 @@ public class Main {
         List<Category> categories = data.getAllCategories();
         for (Category category : categories) {
             System.out.println("ID: " + category.getId() + ", Name: " + category.getName());
+        }
+
+        List<Product> products = data.getProductList(1,3);
+        for (Product product : products) {
+            System.out.println(
+                    "ID: " + product.getId() + ", Name: " + product.getName() + ", Category: " + product.getCategoryName()
+            );
         }
     }
 }
